c8a8d7aa8b8dbb8b0f558754ce20712fde9a1794
silver <qzl.sayyadfar@gmail.com>
2022-01-22 20:35:14 +0330

    priced

--- get-product-price.py
+++ get-product-price.py
@@ -9,6 +9,7 @@ url_list = []
 products_df = pd.read_csv('product-features.csv')
 link_list = list(products_df['link'])
 products_df = pd.read_csv('product-features.csv')
+products_df['price']= ''
 
 headers = {'User-agent': 'Mozilla/5.0'}
 
@@ -19,16 +20,13 @@ for i in range (len(link_list)):
 
     try: 
         price_values = bs_page.find('b', class_="css-0")
-
         print(price_values)
-
         price = price_values.contents
 
         if price != None:
-            url_list.append(link_list[i])
             price = " ".join(price)
             products_df['price'][i] = price
-            print(price)
+
 
         else:
             products_df['price'][i] = "None"
@@ -37,5 +35,6 @@ for i in range (len(link_list)):
     except:
         print(link_list[i])
     
-    products_df.to_csv('new_products_features.csv')
-    time.sleep(180)
+    products_df.to_csv('product-features-priced.csv', index=False)
+    time.sleep(5)
+    
